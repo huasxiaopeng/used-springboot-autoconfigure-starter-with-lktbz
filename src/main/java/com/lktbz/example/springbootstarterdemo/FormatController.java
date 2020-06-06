7311d24561ef790004fccc457785abd4f27ec16c
huasxiaopeng <617387614@qq.com>
2020-06-06 15:30:07 +0800

    first commit

---
+++ src/main/java/com/lktbz/example/springbootstarterdemo/FormatController.java
@@ -0,0 +1,22 @@
+package com.lktbz.example.springbootstarterdemo;
+
+
+import com.lktbz.starter.HelloFormatTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FormatController {
+
+    @Autowired
+    HelloFormatTemplate helloFormatTemplate;
+
+    @GetMapping("/format")
+    public String format(){
+        User user=new User();
+        user.setAge(18);
+        user.setName("Mic");
+        return helloFormatTemplate.doFormat(user);
+    }
+}
